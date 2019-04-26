10cc21350c234f82aa0402953edc676111940cf9
miloudeveen <miloudeveen@gmail.com>
2019-04-26 10:56:37 +0200

    update timer
    Added the minutes and seconds to the countdown. Added a new floating images. Made semantic changes to some classes.

--- timer.js
+++ timer.js
@@ -12,11 +12,18 @@ var timer = setInterval(function() {
         let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
         let secs = Math.floor((t % (1000 * 60)) / 1000);
     
-        document.getElementById("timer-days").innerHTML = days +
-        "<span class='label'>hele lange dagen</span>";
+        document.getElementById("timer-days").innerHTML = days + " " +
+        "<span class='label'>dagen </br></span>";
     
-        document.getElementById("timer-hours").innerHTML = ("0" +hours).slice(-2) +
-        "<span class='label'>uren</span>";
+        document.getElementById("timer-hours").innerHTML = ("0" +hours).slice(-2) + " " +
+        "<span class='label'>uren </br></span>";        
+        
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) + " " +
+        "<span class='label'>minuten </br></span>";
+    
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) + " " +
+        "<span class='label'>seconden</span>";
+
     
     } else {
 
